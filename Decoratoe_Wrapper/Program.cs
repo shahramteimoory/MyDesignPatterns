16b02163293141550496a513c2025a303f3a5e26
shahramteimoory <Darn.shan@yahoo.com>
2024-02-17 09:51:39 +0330

    UseDecorator With out Sorce

--- Decoratoe_Wrapper/Program.cs
+++ Decoratoe_Wrapper/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Decoratoe_Wrapper;
 using Decoratoe_Wrapper.Example2;
+using Decoratoe_Wrapper.UseDecratorWithOutSource;
 
 ConcreateComponent concreateComponent = new ConcreateComponent();
 ConcreateDecoretor concreateDecoretor=new ConcreateDecoretor(concreateComponent); // بجای نمونه از کامپوننت اصلی میتونیم اینجا نمونه از فرزندش بش بدیم
@@ -10,4 +11,7 @@ concreateDecoretor.Operation();
 SendEmail sendEmail = new SendEmail();
 SendEmailDecorator sendEmailDecorator = new SendEmailDecorator(sendEmail);
 sendEmailDecorator.send();
+WithOutSorceExample withOutSorceExample = new WithOutSorceExample();
+string test1= withOutSorceExample.DownloadWeb("https://www.google.com/");
+string test2= withOutSorceExample.DownloadWeb("https://github.com/shahramteimoory"); //متد downloadstring webclient رو به این شکل تغییر دادیم
 Console.ReadLine();
---
+++ Decoratoe_Wrapper/UseDecratorWithOutSource/WithOutSorceExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decoratoe_Wrapper.UseDecratorWithOutSource
+{
+    public class WithOutSorceExample
+    {
+        public string DownloadWeb(string web)
+        {
+            WebClient webClient = new WebClient();
+            //string sorce=  webClient.DownloadString(web);
+            WebClientDecrator webClientDecrator=new WebClientDecrator(webClient);
+            string sorce=webClientDecrator.DownloadString(web);
+            return sorce;
+        }
+
+    }
+    public class WebClientDecrator : WebClient
+    {
+        private readonly WebClient _webClient;
+        public WebClientDecrator(WebClient webClient)
+        {
+            _webClient= webClient;
+        }
+        public string DownloadString(string address)
+        {
+            if (address.StartsWith("https://www.google.com/"))
+            {
+                return _webClient.DownloadString(address);
+            }
+            else
+            {
+                return string.Empty;
+            }
+          
+        }
+    }
+}
