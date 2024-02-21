b497ad38fcdb876e171321dd04f8d49fee6a1479
shahramteimoory <Darn.shan@yahoo.com>
2024-02-21 15:56:53 +0330

    Proxy

---
+++ Proxy/Example1/ISubject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy.Example1
+{
+    public interface ISubject
+    {
+        void DoAction();
+    }
+    internal class RealSubject : ISubject
+    {
+        public void DoAction()
+        {
+            Console.WriteLine("Run DoAction From RealSubject");
+        }
+    }
+    public class Proxy : ISubject
+    {
+        private RealSubject _realSubject;
+        //بجای اینکه تو متد سازنده مقدار دهیش کنیم وقتی متد مد نظرمون رو صدا کردن مقدار دهی میشه
+        public void DoAction()
+        {
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+            _realSubject.DoAction();
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Proxy.Example1;
+
+//انواع پروکسی
+// Virtual Proxy مثل همین مثال
+// Protection Proxy ممکنه بخوایم اول شرایط استفاده از کلاسو برسی کنیم  از این مدل استفاده کنیم قبل نمونه شزایط احراز هویت رو باید چک کرد
+// Remote Proxy زمانی که ما یک شیع راه دور داریم با استفاده از شرایط گرفتنش باید ازش استفاده کنیم  اینجا اول شرایط استفاده شبگه ای رو درست میکنیم
+// LogginProxy واسه لاگ گرفتن
+//CachingProxy مثل بالا عه همش ولی این واسه کش کردنه
+
+ISubject subject = new Proxy.Example1.Proxy();
+if (1==1) //این شرط هرچیزی میتونی باشه و ممکنه برقرار نباشه  اینجوری تو نمونه ای که میسازیم  دیگه منایع نمونه سازی از کلاس RealSubject مصرق نمیشه
+{
+    subject.DoAction();
+}
+Console.ReadLine();
